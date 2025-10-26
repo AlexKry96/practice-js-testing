73bac9affea1ad61acae5b467504527b79dad237
Oleksii Krylov <lehakrylov1996@gmail.com>
2025-10-26 14:27:07 +0100

    03 Test zakresów

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,15 @@
 export default function randomNumber(min, max) {
-    if (min === 1 && max === 1) return 1;
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    throw new Error('Argumenty muszą być liczbami');
+  }
+
+  if (min > max) {
+    throw new Error('Min nie może być większe od max');
+  }
+
+  if (min === max) {
+    return min;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,9 @@ import randomNumber from './app';
 test('zwraca 1, jeśli zakres to od 1 do 1', () => {
   expect(randomNumber(1, 1)).toBe(1);
 });
+
+test('zwracana liczba mieści się w przedziale', () => {
+    const result = randomNumber(1, 10);
+    expect(result).toBeGreaterThanOrEqual(1);
+    expect(result).toBeLessThanOrEqual(10);
+});
