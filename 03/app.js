1ad483c12e233f050352916cdc8d1f158026d655
Oleksii Krylov <lehakrylov1996@gmail.com>
2025-10-26 14:34:23 +0100

    03 Obsługa błędnych argumentów

--- 03/app.js
+++ 03/app.js
@@ -1,15 +1,15 @@
 export default function randomNumber(min, max) {
-  if (typeof min !== 'number' || typeof max !== 'number') {
-    throw new Error('Argumenty muszą być liczbami');
-  }
+    if (typeof min !== 'number' || typeof max !== 'number') {
+        throw new Error('Argumenty muszą być liczbami');
+    }
 
-  if (min > max) {
-    throw new Error('Min nie może być większe od max');
-  }
+    if (min > max) {
+        throw new Error('Niepoprawny zakres: min nie może być większe od max');
+    }
 
-  if (min === max) {
-    return min;
-  }
+    if (min === max) {
+        return min;
+    }
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
