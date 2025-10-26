9b01d655abc2ba07bb4109f4a952d13f41d3b3f1
Oleksii Krylov <lehakrylov1996@gmail.com>
2025-10-26 14:43:32 +0100

    04 Done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,35 @@
 export default class User {
+    constructor({ email, password }) {
+        if (!this.isEmailValid(email)) {
+            throw new Error('Invalid email');
+        }
 
+        if (!this.isPasswordValid(password)) {
+            throw new Error('Invalid password');
+        }
+
+        this.email = email;
+        this.password = password;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    login() {
+        return this.email.includes('devmentor.pl');
+    }
+
+    isEmailValid(email) {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(email);
+    }
+
+    isPasswordValid(password) {
+        return typeof password === 'string' && password.length >= 6;
+    }
 }
